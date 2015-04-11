2335cc73a14db16f280e23b39e2c04c7112b743a
Alexey Morozov <alexey.morozov.is@gmail.com>
2015-04-11 17:32:30 +0300

    Refactor: start game

--- lib/blackjack/game.rb
+++ lib/blackjack/game.rb
@@ -5,8 +5,17 @@ module Blackjack
     end
 
     def start(deck)
-      @printer.puts('Welcome to Blackjack!')
+      welcome
+      initial_deal(deck)
+      show_hands
+      stand if @player_hand.score == 21
+    end
 
+    def welcome
+      @printer.puts('Welcome to Blackjack!')
+    end
+
+    def initial_deal(deck)
       @deck = deck.split.reverse
       @player_hand = Hand.new
       @dealer_hand = Hand.new
@@ -15,16 +24,18 @@ module Blackjack
         @player_hand << @deck.pop
         @dealer_hand << @deck.pop
       end
+    end
 
+    def show_hands
       @printer.puts("Dealer's hand: #{@dealer_hand}. Score: #{@dealer_hand.score}.")
       @printer.puts("Your hand: #{@player_hand}. Score: #{@player_hand.score}.")
+    end
 
-      if @player_hand.score == 21
-        if @dealer_hand.score < 21
-          @printer.puts("You win!")
-        else
-          @printer.puts("You push!")
-        end
+    def stand
+      if @dealer_hand.score < 21
+        @printer.puts("You win!")
+      else
+        @printer.puts("You push!")
       end
     end
   end
