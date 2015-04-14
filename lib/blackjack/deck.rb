4ae8f19edcaaa3557dfbe28ab69863d734f737ef
Alexey Morozov <alexey.morozov.is@gmail.com>
2015-04-14 21:32:30 +0300

    Use a shuffled deck when run at terminal

--- lib/blackjack/deck.rb
+++ lib/blackjack/deck.rb
@@ -3,5 +3,19 @@ module Blackjack
     def self.create_from_string(card_codes)
       card_codes.split.reverse.map { |code| Card.new(code) }
     end
+
+    def self.random_deck
+      suits = %w(♠ ♥ ♦ ♣)
+      ranks = %w(A K Q J T 9 8 7 6 5 4 3 2)
+
+      cards = []
+      suits.each do |suit|
+        ranks.each do |rank|
+          cards << Card.new(rank + suit)
+        end
+      end
+
+      cards.shuffle
+    end
   end
 end
