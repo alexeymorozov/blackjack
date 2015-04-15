1380ae63c4acdf43a1931d6c028d45a60c59cf36
Alexey Morozov <alexey.morozov.is@gmail.com>
2015-04-15 12:52:17 +0300

    Refactor creation of a new shuffled deck

--- lib/blackjack/deck.rb
+++ lib/blackjack/deck.rb
@@ -4,18 +4,10 @@ module Blackjack
       card_codes.split.reverse.map { |code| Card.new(code) }
     end
 
-    def self.random_deck
+    def self.create_standard_deck
       suits = %w(♠ ♥ ♦ ♣)
       ranks = %w(A K Q J T 9 8 7 6 5 4 3 2)
-
-      cards = []
-      suits.each do |suit|
-        ranks.each do |rank|
-          cards << Card.new(rank + suit)
-        end
-      end
-
-      cards.shuffle
+      ranks.product(suits).map { |rank, suit| Card.new(rank + suit) }
     end
   end
 end
