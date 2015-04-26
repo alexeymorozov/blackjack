0b6745f9625b7290b950974b4aaf5fce60708805
Alexey Morozov <alexey.morozov.is@gmail.com>
2015-04-26 21:42:02 +0300

    Simplify the implementation of HandList

--- lib/blackjack/hand_list.rb
+++ lib/blackjack/hand_list.rb
@@ -1,35 +1,23 @@
 module Blackjack
-  class HandList
-    def initialize(hands)
-      @hands = hands
-    end
-
+  class HandList < Array
     def all_finished?
-      @hands.all? { |hand| hand.finished? }
+      all? { |hand| hand.finished? }
     end
 
     def all_busted?
-      @hands.all? { |hand| hand.busted? }
+      all? { |hand| hand.busted? }
     end
 
     def all_have_blackjacks?
-      @hands.all? { |hand| hand.has_blackjack? }
+      all? { |hand| hand.has_blackjack? }
     end
 
     def all_have_bets?
-      @hands.all? { |hand| hand.bet }
+      all? { |hand| hand.bet }
     end
 
     def dealt?
-      @hands.any? { |hand| hand.dealt? }
-    end
-
-    def first
-      @hands.first
-    end
-
-    def each(&block)
-      @hands.each(&block)
+      any? { |hand| hand.dealt? }
     end
   end
 end
